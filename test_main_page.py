231c861f45f648966526dfe1852e9552f22e790d
ale-mikhailova <gvenvivarr@yandex.ru>
2024-01-22 14:59:32 +0300

    exception check added

--- test_main_page.py
+++ test_main_page.py
@@ -4,5 +4,6 @@ from selenium.webdriver.common.by import By
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
-    page.open() 
-    page.go_to_login_page() 
+    page.open()
+    page.go_to_login_page()
+    page.should_be_login_link()
